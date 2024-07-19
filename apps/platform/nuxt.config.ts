3692da5fb208b7445081916764f2606b735eb5ed
David Kopka <david.kopka@adesso.de>
2024-07-19 14:32:18 +0200

    🔧 Add turbo config, edit nuxt config & docs package.json

--- apps/platform/nuxt.config.ts
+++ apps/platform/nuxt.config.ts
@@ -2,7 +2,10 @@
 export default defineNuxtConfig({
   compatibilityDate: "2024-04-03",
   devtools: { enabled: true },
+  telemetry: false,
+  
   modules: ["@nuxt/eslint", "@nuxtjs/tailwindcss"],
+
   future: {
     compatibilityVersion: 4,
   },
