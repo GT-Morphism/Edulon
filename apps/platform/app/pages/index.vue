13a05230af59ab6d9b5a340cd0cbe40e45a2e54b
David Kopka <david.kopka@adesso.de>
2024-07-24 17:49:15 +0200

    🚧 Add course detail page

--- apps/platform/app/pages/index.vue
+++ apps/platform/app/pages/index.vue
@@ -1,16 +1,5 @@
-<script setup lang="ts">
-  const lectures = await getLectures();
-</script>
-
 <template>
   <div>
     <h1>Home</h1>
-    <p>All about you</p>
-    <ul>
-      <li v-for="lecture in lectures" :key="lecture.title">
-        {{ lecture.title }}
-        <span>{{ lecture.author }}</span>
-      </li>
-    </ul>
   </div>
 </template>
---
+++ apps/platform/app/pages/kurse/[slug].vue
@@ -0,0 +1,24 @@
+<script lang="ts" setup>
+  const DATA: globalThis.Ref<{
+    id: string;
+    slug?: string;
+    name: string;
+    thumbnail: string;
+    created: string;
+    updated: string;
+  }> = ref({
+    id: "",
+    name: "",
+    thumbnail: "",
+    created: "24.07.2024T09:30:15.000Z",
+    updated: "24.07.2024T09:30:15.000Z",
+  });
+
+  onMounted(() => {
+    console.log(useRoute().params.slug);
+    DATA.value = {
+      ...DATA.value,
+      slug: useRoute().params.slug?.toString(),
+    };
+  });
+</script>
