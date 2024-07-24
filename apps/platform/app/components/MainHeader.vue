64c205e95eb6a2876235ef64d8d6461bf0beae1d
David Kopka <david.kopka@adesso.de>
2024-07-24 15:11:28 +0200

    :construction: static detail page

--- apps/platform/app/components/MainHeader.vue
+++ apps/platform/app/components/MainHeader.vue
@@ -1,9 +1,11 @@
 <template>
-  <span> Logo </span>
-  <nav class="bg-blue-500 py-2 text-white">
-    <ul class="flex justify-center gap-x-4">
-      <li><NuxtLink to="/">Home</NuxtLink></li>
-      <li><NuxtLink to="/about">About</NuxtLink></li>
-    </ul>
-  </nav>
+  <header class="flex justify-between py-2">
+    <span class="bg-edulon-500"> Logo </span>
+    <nav class="bg-blue-500 py-2 text-white">
+      <ul class="flex justify-center gap-x-4">
+        <li><NuxtLink to="/">Home</NuxtLink></li>
+        <li><NuxtLink to="/about">About</NuxtLink></li>
+      </ul>
+    </nav>
+  </header>
 </template>
