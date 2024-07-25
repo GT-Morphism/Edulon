f37a20e2a7c8bc72fbd82e760c01063fe4151963
David Kopka <david.kopka@agilido.com>
2024-07-25 18:53:39 +0200

    🚀 Edit navigation; create routes

--- apps/platform/app/components/MainHeader.vue
+++ apps/platform/app/components/MainHeader.vue
@@ -1,23 +1,50 @@
 <template>
-  <header class="bg-surface-500 flex h-12 justify-between px-6 py-2">
+  <header
+    class="dark:bg-surface-500 flex h-14 justify-between border-b border-b-neutral-400 bg-neutral-50 px-6 py-2"
+  >
     <NuxtLink to="/#">
       <span
-        class="bg-edulon-500 flex h-7 w-36 items-center justify-center text-xs text-black"
+        class="bg-edulon-500 flex w-36 items-center justify-center rounded py-2 text-black"
       >
-        Logo
+        <span class="me-2 h-5 w-5 rounded-full bg-white" />
+        AD.ZUBI
       </span>
     </NuxtLink>
 
-    <nav class="text-white">
-      <ul class="flex items-center justify-center gap-x-6 text-xs">
-        <li><NuxtLink to="/kurse">Kurse</NuxtLink></li>
-        <li><NuxtLink to="/meine-kurse">Meine Kurse</NuxtLink></li>
-        <li><NuxtLink to="/challenge">Challenge</NuxtLink></li>
-        <li><NuxtLink to="/lernressourcen">Lernressourcen</NuxtLink></li>
-        <li><NuxtLink to="/dozent-werden">Dozent werden</NuxtLink></li>
-        <li><NuxtLink to="/ausbilderteam">Ausbilderteam</NuxtLink></li>
+    <nav class="flex items-center dark:text-white">
+      <ul class="flex items-center justify-center gap-x-6">
         <li>
-          <NuxtLink to="/profil" class="flex items-center gap-x-2">
+          <NuxtLink to="/kurse" exact-active-class="text-edulon-500">
+            Kurse
+          </NuxtLink>
+        </li>
+        <li>
+          <NuxtLink to="/meine-kurse" exact-active-class="text-edulon-500">
+            Meine Kurse
+          </NuxtLink>
+        </li>
+        <li>
+          <NuxtLink to="/challenge" exact-active-class="text-edulon-500">
+            Challenge
+          </NuxtLink>
+        </li>
+        <li>
+          <NuxtLink to="/lernressourcen" exact-active-class="text-edulon-500">
+            Lernressourcen
+          </NuxtLink>
+        </li>
+        <li>
+          <NuxtLink to="/dozent-werden" exact-active-class="text-edulon-500">
+            Dozent werden
+          </NuxtLink>
+        </li>
+        <li>
+          <NuxtLink to="/ausbilderteam" exact-active-class="text-edulon-500">
+            Ausbilderteam
+          </NuxtLink>
+        </li>
+        <li>
+          <NuxtLink to="/profil" class="flex items-center gap-x-2 text-xs">
             <UAvatar
               src="https://avatars.githubusercontent.com/u/72008643?v=4"
             />
@@ -25,7 +52,7 @@
           </NuxtLink>
         </li>
         <li>
-          <UButton size="2xs">Login</UButton>
+          <UButton size="2xs"> Login </UButton>
         </li>
       </ul>
     </nav>
---
+++ apps/platform/app/pages/kurse/index.vue
@@ -0,0 +1,5 @@
+<template>
+  <div>
+    Kurse
+  </div>
+</template>
