894dbfa785d82fc33bbd7c5b250d32ac7ea0ab34
TarekAmmar1993 <tarak.ammar@hotmail.com>
2024-07-31 15:07:49 +0200

    ♻️ delete unnecesarry files + refactor css code

--- apps/platform/app/components/Card.vue
+++ apps/platform/app/components/Card.vue
@@ -1,20 +1,25 @@
+<script setup lang="ts">
+  defineProps({
+    title: String,
+    description: String,
+  });
+</script>
+
 <template>
-  <article class="grid max-w-80 rounded-lg bg-white">
+  <article
+    class="grid h-full max-w-80 grid-rows-[auto_1fr_auto] rounded-lg bg-white"
+  >
     <div class="h-28 bg-gray-600"></div>
-    <div class="flex flex-col gap-4 p-6 text-black">
-      <header class="text-xl font-medium leading-8 md:text-2xl">
-        Kurse 03
+    <div class="flex flex-col gap-4 p-6 dark:text-black">
+      <header v-if="title" class="text-xl/8 font-medium md:text-2xl">
+        {{ title }}
       </header>
-      <p class="text-sm font-normal leading-6 md:text-base">
-        Maecenas nec odio et ante tincidunt tempus. Donec vitae sapien ut libero
-        venenatis faucibus. Nullam quis ante. Etiam sit amet orci eget eros
-        faucibus tincidunt. Duis leo. Sed fringilla mauris sit amet nibh. Donec
-        sodal
+      <p class="text-sm/6 md:text-base">
+        {{ description }}
       </p>
     </div>
-    <UButton
-      class="mx-6 my-8 w-fit justify-self-end rounded-md px-4 py-3 text-sm font-medium leading-5"
-      >Kurs Ansehen</UButton
-    >
+    <div class="flex justify-end border-t px-6 py-8">
+      <UButton to="#" size="lg"> Kurs Ansehen </UButton>
+    </div>
   </article>
 </template>
