c7247777f3401e1a623937e014bfa5df7f715d96
TarekAmmar1993 <tarak.ammar@hotmail.com>
2024-08-01 12:17:59 +0200

    ✨  add hero component

---
+++ apps/platform/app/components/Hero.vue
@@ -0,0 +1,30 @@
+<script setup lang="ts">
+  defineProps<{
+    title: string;
+    description?: string;
+    button: {
+      buttonText: string;
+      buttonTarget: string;
+      buttonUrl: string;
+    };
+  }>();
+</script>
+<template>
+  <div
+    class="flex h-[560px] items-center justify-center overflow-auto bg-[url('/assets/hero.png')] md:block"
+  >
+    <div
+      class="mx-7 bg-white p-8 md:mx-0 md:ml-12 md:mt-14 md:max-w-[40%] dark:text-black"
+    >
+      <p class="mb-4 text-4xl/10 font-medium">
+        {{ title }}
+      </p>
+      <p class="pb-7 leading-7" v-if="description">
+        {{ description }}
+      </p>
+      <UButton :to="button.buttonUrl" size="lg" :target="button.buttonTarget">{{
+        button.buttonText
+      }}</UButton>
+    </div>
+  </div>
+</template>
--- apps/platform/app/pages/playground.vue
+++ apps/platform/app/pages/playground.vue
@@ -16,12 +16,68 @@
         "Maecenas nec odio et ante tincidunt tempus. Donec vitae sapien ut libero venenatis faucibus. Nullam quis ante. Etiam sit amet orci eget eros faucibus tincidunt. ",
     },
   ];
+  const heroes = [
+    {
+      title: " 1 Hero consequat massa quis enim.",
+      description:
+        "Maecenas nec odio et ante tincidunt tempus. Donec vitae sapien ut libero venenatis faucibus. Nullam quis ante. Etiam sit amet orci eget eros faucibus tincidunt. Duis leo. Sed fringilla mauris sit amet nibh. Donecsodal",
+      button: {
+        buttonText: "Ansehen",
+        buttonTarget: "_blank",
+        buttonUrl: "#",
+      },
+    },
+    {
+      title: "2 Hero consequat massa quis enim.",
+      description: "Maecenas nec odio et ante tincidunt tempus.",
+      button: {
+        buttonText: "Ansehen",
+        buttonTarget: "",
+        buttonUrl: "#",
+      },
+    },
+    {
+      title: "3 Hero consequat massa quis enim.",
+      description:
+        "Maecenas nec odio et ante tincidunt tempus. Donec vitae sapien ut libero venenatis faucibus. Nullam quis ante. Etiam sit amet orci eget eros faucibus tincidunt. ",
+      button: {
+        buttonText: "Ansehen",
+        buttonTarget: "_blank",
+        buttonUrl: "#",
+      },
+    },
+  ];
 </script>
 
 <template>
-  <div class="grid grid-cols-3 gap-4 py-4">
-    <div v-for="card in cards">
-      <Card :title="card.title" :description="card.description" />
+  <section>
+    <h2>Card Component</h2>
+    <div class="grid grid-cols-3 gap-4">
+      <div v-for="card in cards">
+        <Card :title="card.title" :description="card.description" />
+      </div>
     </div>
-  </div>
+  </section>
+  <section>
+    <h2>Hero Component</h2>
+    <div class="flex flex-col gap-4">
+      <div v-for="hero in heroes">
+        <Hero
+          :title="hero.title"
+          :description="hero.description"
+          :button="hero.button"
+        />
+      </div>
+    </div>
+  </section>
 </template>
+
+<style>
+  section {
+    @apply mb-24;
+  }
+
+  h2 {
+    @apply text-primary mb-4 text-2xl/tight;
+  }
+</style>
