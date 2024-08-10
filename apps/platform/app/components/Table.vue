e3259172d3ea7f0fa1cdf8895d0fdad4dd32fce5
TarekAmmar1993 <tarak.ammar@hotmail.com>
2024-08-10 15:01:18 +0200

    ✨ add the filters functionnalites

--- apps/platform/app/components/Table.vue
+++ apps/platform/app/components/Table.vue
@@ -1,98 +1,43 @@
 <script setup lang="ts">
-  const people = [
+  const courses = [
     {
-      title: "adZubi - Self Learning 1",
+      title: "HTML",
       lehrjahr: "1 Lehrjahr",
       zeit: "1 Woche",
-      tag: "Onboarding",
+      tag: "onboarding",
       fachrichtung: "designer",
       status: "offen",
     },
     {
-      title: "adZubi - Self Learning 2",
+      title: "CSS",
       lehrjahr: "1 Lehrjahr",
       zeit: "1 Woche",
-      tag: "Onboarding",
+      tag: "onboarding",
       fachrichtung: "fachinformatiker",
       status: "abgeschlossen",
     },
     {
-      title: "adZubi - Self Learning 3",
+      title: "Javascript",
       lehrjahr: "1 Lehrjahr",
-      zeit: "1 Woche",
-      tag: "Onboarding",
+      zeit: "2 Wochen",
+      tag: "farbelehre",
       fachrichtung: "designer",
       status: "abgeschlossen",
     },
     {
-      title: "adZubi - Self Learning 4",
-      lehrjahr: "1 Lehrjahr",
+      title: "Vue Js",
+      lehrjahr: "2 Lehrjahr",
       zeit: "1 Woche",
       tag: "Onboarding",
       fachrichtung: "fachinformatiker",
       status: "offen",
     },
   ];
-  const subjects = [
-    {
-      key: "fachinformatiker",
-      label: "Fachinformatiker",
-      value: "fachinformatiker",
-    },
-    {
-      key: "designer",
-      label: "Designer",
-      value: "designer",
-    },
-  ];
-  const status = [
-    {
-      key: "abgeschlossen",
-      label: "Abgeschlossen",
-      value: true,
-    },
-    {
-      key: "offen",
-      label: "Offen",
-      value: false,
-    },
-  ];
-  const days = [
-    {
-      key: "onboarding",
-      label: "Onboarding",
-      value: "onboarding",
-    },
-    {
-      key: "farbelehre",
-      label: "Farbelehre",
-      value: "farbelehre",
-    },
-  ];
-  const time = [
-    {
-      key: 1,
-      label: "1 Woche",
-      value: 1,
-    },
-    {
-      key: 2,
-      label: "2 Wochen",
-      value: 2,
-    },
-  ];
-  const levels = [
-    {
-      key: 1,
-      label: "1 Lehrjahr",
-      value: 1,
-    },
-    {
-      key: 2,
-      label: "2 Lehrjahr",
-      value: 2,
-    },
-  ];
+  const subjects = ["fachinformatiker", "designer"];
+  const status = ["abgeschlossen", "offen"];
+  const days = ["onboarding", "farbelehre"];
+  const time = ["1 Woche", "2 Wochen"];
+  const levels = ["1 Lehrjahr", "2 Lehrjahr"];
   const columns = [
     {
       key: "title",
@@ -124,35 +69,88 @@
       label: "Status",
       sortable: true,
     },
+    {
+      key: "action",
+      label: "Action",
+    },
   ];
 
-  const selectedSubject = ref([]);
-  const selectedStatus = ref([]);
-  const selectedDay = ref([]);
-  const selectedTime = ref([]);
-  const selectedLevel = ref([]);
-  const selected = ref([people[0]]);
-  const q = ref("");
-  const filteredRows = computed(() => {
-    if (!q.value) {
-      return people;
-    }
+  const selectedSubject = ref<string[]>([]);
+  const selectedStatus = ref<string[]>([]);
+  const selectedDay = ref<string[]>([]);
+  const selectedTime = ref<string[]>([]);
+  const selectedLevel = ref<string[]>([]);
+  const selected = ref([courses[0]]);
+  const searchQuery = ref("");
+  const selectedColumns = ref(columns);
 
-    return people.filter((person) => {
-      return Object.values(person).some((value) => {
-        return String(value).toLowerCase().includes(q.value.toLowerCase());
-      });
-    });
+  const columnsTable = computed(() =>
+    columns.filter((column) => selectedColumns.value.includes(column)),
+  );
+
+  const filteredRows = computed(() => {
+    if (
+      !searchQuery.value &&
+      selectedStatus.value.length === 0 &&
+      selectedSubject.value.length === 0 &&
+      selectedDay.value.length === 0 &&
+      selectedTime.value.length === 0 &&
+      selectedLevel.value.length === 0
+    ) {
+      return courses;
+    } else {
+      return courses
+        .filter((course) => {
+          return Object.values(course).some((value) => {
+            return String(value)
+              .toLowerCase()
+              .includes(searchQuery.value.toLowerCase());
+          });
+        })
+        .filter((course) => {
+          if (selectedStatus.value.length === 0) {
+            return course;
+          }
+          return selectedStatus.value.includes(course.status);
+        })
+        .filter((course) => {
+          if (selectedSubject.value.length === 0) {
+            return course;
+          }
+          return selectedSubject.value.includes(course.fachrichtung);
+        })
+        .filter((course) => {
+          if (selectedDay.value.length === 0) {
+            return course;
+          }
+          return selectedDay.value.includes(course.tag);
+        })
+        .filter((course) => {
+          if (selectedTime.value.length === 0) {
+            return course;
+          }
+          return selectedTime.value.includes(course.zeit);
+        })
+        .filter((course) => {
+          if (selectedLevel.value.length === 0) {
+            return course;
+          }
+          return selectedLevel.value.includes(course.lehrjahr);
+        });
+    }
   });
 </script>
 
 <template>
   <section>
+    <header>
+      <p class="mb-9">Kurse Liste</p>
+    </header>
     <div
       class="flex gap-4 border-b border-gray-200 px-3 py-3.5 dark:border-gray-700"
     >
       <UInput
-        v-model="q"
+        v-model="searchQuery"
         placeholder="Search..."
         icon="i-heroicons-magnifying-glass-20-solid"
         class="w-80"
@@ -163,34 +161,74 @@
         :options="subjects"
         multiple
         placeholder="Fachrichtung"
-      />
+      >
+      </USelectMenu>
+
       <USelectMenu
         v-model="selectedStatus"
         :options="status"
         multiple
         placeholder="Status"
+        class="w-32"
       />
       <USelectMenu
         v-model="selectedDay"
         :options="days"
         multiple
         placeholder="Tag"
+        class="w-32"
       />
       <USelectMenu
         v-model="selectedTime"
         :options="time"
         multiple
         placeholder="Zeit"
+        class="w-32"
       />
       <USelectMenu
         v-model="selectedLevel"
         :options="levels"
         multiple
         placeholder="Lehrjahr"
+        class="w-32"
       />
+
+      <USelectMenu
+        v-model="selectedColumns"
+        :options="columns"
+        multiple
+        class="ml-auto"
+      >
+        <UButton icon="i-heroicons-view-columns" color="gray" size="sm">
+          Listansicht
+        </UButton>
+      </USelectMenu>
     </div>
 
-    <UTable v-model="selected" :rows="filteredRows" :columns="columns">
+    <UTable v-model="selected" :rows="filteredRows" :columns="columnsTable">
+      <template #status-data="{ row }">
+        <UBadge
+          size="xs"
+          :label="row.status === 'abgeschlossen' ? 'abgeschlossen' : 'offen'"
+          :color="row.status === 'abgeschlossen' ? 'emerald' : 'white'"
+          variant="subtle"
+        />
+      </template>
+      <template #fachrichtung-data="{ row }">
+        <UBadge
+          size="xs"
+          :label="row.fachrichtung"
+          :color="'white'"
+          variant="subtle"
+        />
+      </template>
+      <template #tag-data="{ row }">
+        <UBadge size="xs" :label="row.tag" :color="'white'" variant="subtle" />
+      </template>
+
+      <template #action-data="{ row }">
+        <UButton>Ansehen</UButton>
+      </template>
     </UTable>
   </section>
 </template>
